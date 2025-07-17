e3c36ecee168fe20fd996ffcd8fb92f60d541e9c
Max <bernardmaxence09@gmail.com>
2025-07-17 22:14:05 +0200

    Fix : correction de la logique de vérification de valeur, et test d'ajout de la deuxième question

--- exercice1/calculatrice.js
+++ exercice1/calculatrice.js
@@ -1,8 +1,5 @@
 const readline = require('readline');
 
-let num1 = 0
-let num2 = 0
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -27,7 +24,11 @@ function division(param1, param2) {
 
 rl.question('Entrez le premier chiffre : \n', (answer) => {
   const num1 = parseInt(answer)
-  if (isNaN(num1) == true) {
+  if (!isNaN(num1)) {
+    rl.question('Entrez le deuxième chiffre : \n', (answer) => {
+      console.log('deuxieme question atteinte')
+    })
+  }else {
     rl.setPrompt('Veuillez entrer une valeur numérique \n')
     rl.prompt()
     rl.on('line', (answer) => {
@@ -35,16 +36,11 @@ rl.question('Entrez le premier chiffre : \n', (answer) => {
       if(isNaN(num1)) {
         rl.setPrompt('Veuillez entrer une valeur numérique \n')
         rl.prompt()
-      }
-      else {
-        return num1 && rl.close()
       }  
     })
   }
-  else {
-    return num1 && rl.close()
-  }
 })
+
 
 
 

b936fc419d2008955e10f34d19ab8df6a58d5822
Max <bernardmaxence09@gmail.com>
2025-07-18 22:01:10 +0200

    Fix : refonte de la calculatrice en reutilisant le code déjà existant plutot que de reinventer la roue avec des rl.on(line)

--- exercice1/calculatrice.js
+++ exercice1/calculatrice.js
@@ -25,113 +25,6 @@ function division(param1, param2) {
 
 
 /*
-Je garde ça au cas ou je foire le reste
-
-rl.question('Entrez le premier chiffre : \n', (answer) => {
-  const num1 = parseInt(answer)
-  if (!isNaN(num1)) {
-    rl.question('Entrez le deuxième chiffre : \n', (answer) => {
-      const num2 = parseInt(answer)
-      if (!isNaN(num2)) {
-        rl.question('Saisissez un opérateur \n', (answer) => {
-          if (validOperators.includes(answer)) {
-            if(answer == '+') {
-              const operation = addition(num1, num2)
-              console.log(`${num1} + ${num2} = ${operation}`)
-              rl.close()
-            }
-            else if(answer == '-') {
-              const operation = soustraction(num1, num2)
-              console.log(`${num1} - ${num2} = ${operation}`)
-              rl.close()
-            }
-            else if(answer == '*') {
-              const operation = multiplication(num1, num2)
-              console.log(`${num1} * ${num2} = ${operation}`)
-              rl.close()
-            }
-            else if(answer == '/') {
-              const operation = division(num1, num2)
-              console.log(`${num1} / ${num2} = ${operation}`)
-              rl.close()
-            }
-          }else {
-            rl.setPrompt('L\'opérateur saisi n\'est pas correct, réessayez \n')
-            rl.prompt()
-            rl.on('line', (answer) => {
-              if (validOperators.includes(answer)) {
-                if(answer == '+') {
-                  const operation = addition(num1, num2)
-                  console.log(`${num1} + ${num2} = ${operation}`)
-                  rl.close()
-                }
-                else if(answer == '-') {
-                  const operation = soustraction(num1, num2)
-                  console.log(`${num1} - ${num2} = ${operation}`)
-                  rl.close()
-                }
-                else if(answer == '*') {
-                  const operation = multiplication(num1, num2)
-                  console.log(`${num1} * ${num2} = ${operation}`)
-                  rl.close()
-                }
-                else if(answer == '/') {
-                  const operation = division(num1, num2)
-                  console.log(`${num1} / ${num2} = ${operation}`)
-                  rl.close()
-                }
-              }else {
-                rl.setPrompt('L\'opérateur saisi n\'est pas correct, réessayez \n')
-                rl.prompt()
-              }
-            })
-          }
-        })
-      }else {
-        rl.setPrompt('Veuillez entrer une valeur numérique \n')
-        rl.prompt()
-        rl.on('line', (answer) => {
-          const num2 = parseInt(answer)
-          if(isNaN(num2)) {
-            rl.setPrompt('Veuillez entrer une valeur numérique \n')
-            rl.prompt()
-          }  
-        })
-      }
-    })
-  }else {
-    rl.setPrompt('Veuillez entrer une valeur numérique \n')
-    rl.prompt()
-    rl.on('line', (answer) => {
-      const num1 = parseInt(answer)
-      if(isNaN(num1)) {
-        rl.setPrompt('Veuillez entrer une valeur numérique \n')
-        rl.prompt()
-      }else {
-        rl.question('Entrez le deuxième chiffre : \n', (answer) => {
-          const num2 = parseInt(answer)
-          if (!isNaN(num2)) {
-            rl.question('Saisissez un opérateur', (answer) => {
-              console.log('on est à l\'opérateur')
-            })
-          }else {
-            rl.setPrompt('Veuillez entrer une valeur numérique \n')
-            rl.prompt()
-            rl.on('line', (answer) => {
-              const num2 = parseInt(answer)
-              if(isNaN(num2)) {
-                rl.setPrompt('Veuillez entrer une valeur numérique \n')
-                rl.prompt()
-              }  
-            })
-          }
-        })
-      }  
-    })
-  }
-})
-*/
-
 let operateur = ""
 
 function premiereQuestion() {
@@ -202,19 +95,12 @@ function deuxiemeNombre() {
     num2 = parseInt(answer)
     if (!isNaN(num2)) {
       return num2 && calcul(num1, num2, operateur)
-    }else {
-      rl.setPrompt('Veuillez entrer une valeur numérique \n')
+    }else if(num2 == 0 && operateur == "/") {
+      rl.setPrompt('Division par 0 impossible, saisissez un autre nombre')
       rl.prompt()
-      rl.on('line', (answer) => {
-        num2 = parseInt(answer)
-        if(isNaN(num2)) {
-          rl.setPrompt('Veuillez entrer une valeur numérique \n')
-          rl.prompt()
-        }else {
-          return num2 && calcul(num1, num2, operateur)
-        }
-      })
+      return deuxiemeNombre()
     }
+    deuxiemeNombre()
   })
 }
 
@@ -238,14 +124,118 @@ function calcul(param1, param2, param3) {
   return resultat && rl.close()
 }
 
-premiereQuestion()
-
 rl.on('close', () => {
   console.log(`le resultat est ${resultat}`)
+  resultat = 0
 })
 
+function calculatrice() {
+  premiereQuestion()
+}
 
+calculatrice()
+*/
 
+let operateur = " "
+
+function calculatrice() {
+  rl.question('Choisissez une option : \n 1.Addition \n 2.Soustraction \n 3.Multiplication \n 4.Division \n 5.Quitter \n', (answer) => {
+    switch (answer) {
+
+      case "1":
+        operateur = "+"
+        console.log('Vous choisissez l\'addition')
+        premierNombre()
+        break;
+
+      case "2":
+        operateur = "-"
+        console.log('Vous choisissez la soustraction')
+        premierNombre()
+        break;
+
+      case "3":
+        operateur = "*"
+        console.log('Vous choisissez la multiplication')
+        premierNombre()
+        break;
+
+      case "4":
+        operateur = "/"
+        console.log('Vous choisissez la division')
+        premierNombre()
+        break;
+
+      case "5":
+        console.log('Vous choisissez de quitter')
+        rl.close()
+        break;
+    
+      default:
+        console.log('Choix invalide, veuillez réssayer')
+        calculatrice()
+        break;
+    }
+  })
+}
+
+let num1 = 0
+
+function premierNombre() {
+  rl.question('Entrez le premier nombre : ', (answer) => {
+    num1 = parseInt(answer)
+    if (!isNaN(num1)) {
+      return num1 && deuxiemeNombre()
+    } else {
+      console.log('Veuillez entrer un nombre valide.')
+      premierNombre()
+    }
+  })
+}
+
+let num2 = 0
+
+function deuxiemeNombre() {
+  rl.question('Entrez le deuxième nombre : ', (answer) => {
+    num2 = parseInt(answer)
+    if (isNaN(num2)) {
+      console.log('Veuillez entrer un nombre valide.')
+      deuxiemeNombre()
+    } else if (operateur == '/' && num2 == 0) {
+      console.log("Vous ne pouvez pas diviser par zéro, veuillez entrer un autre nombre")
+      deuxiemeNombre()
+    } else {
+      return num2 && calcul(num1, num2, operateur)
+    }
+  })
+}
+
+let resultat = 0
+
+function calcul(param1, param2, param3) {
+  switch (param3) {
+
+    case "+":
+      resultat = addition(num1, num2)
+      break;
+
+    case "-":
+      resultat = soustraction(num1, num2)
+      break;
+
+    case "*":
+      resultat = multiplication(num1, num2)
+      break;
+
+    case "/":
+      resultat = division(num1, num2)
+      break;
+  }
+  console.log(`Le resultat est ${resultat}`)
+  rl.close()
+}
+
+calculatrice()
 
 
 
